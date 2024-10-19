88d4f2ae28be5f0b31bab4ad1b779ef81929dc96
Zhenya Luzhanski <luzhanskizhenya@gmail.com>
2024-10-19 15:25:07 +0300

    commit n6

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,8 +1,5 @@
 import Swiper from "swiper";
 import { Navigation , EffectCube} from 'swiper/modules';
-// import 'swiper/css';
-// import 'swiper/css/navigation';
-// import 'swiper/css/bundle';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/bundle';
